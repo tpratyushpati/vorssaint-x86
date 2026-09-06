bef775e42fa0342fe52e3c1ee49be42c978ae7d1
tpratyushpati <tpratyushpati@gmail.com>
2026-09-06 09:57:31 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -59,7 +59,8 @@ FAN_HELPER_ID="$APP_BUNDLE_ID.fan-control"
 # Sources/NowPlayingAdapter. Staged under Contents/Frameworks, signed on its own.
 NOW_PLAYING_ADAPTER_ID="$APP_BUNDLE_ID.now-playing"
 NOW_PLAYING_ADAPTER="libVorssaintNowPlaying.dylib"
-TARGET="arm64-apple-macosx14.0"
+ARCH="${VORSSAINT_ARCH:-$(uname -m)}"
+TARGET="$ARCH-apple-macosx14.0"
 ENTITLEMENTS="Resources/Vorssaint.entitlements"
 LEGACY_IDENTITY="Vorssaint Utils Signing"
 
